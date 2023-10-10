00243d87e9b05da752e26c6b8e07386e3fb79a42
Mark Thompson <mthompson@bitovi.com>
2023-10-10 11:34:53 -0700

    woo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Place My Order App: Coming Soon!</h1>
+    </>
+  )
+}
+
+export default App
